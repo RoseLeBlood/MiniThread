3588386ac5a66add6f580063bc8da6ad4ea740aa
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2020-07-02 17:47:31 +0200

    Add Platrormio support to BasicExample

---
+++ example/00_BasicExample/include/task.hpp
@@ -0,0 +1,15 @@
+#ifndef __EXAMPLE_MNTHREAD_HALLOWORLD_H__
+#define __EXAMPLE_MNTHREAD_HALLOWORLD_H__
+
+#include <libmn.h>
+
+class helloWorld_task : public mthread {
+public:
+  helloWorld_task();
+
+  virtual void* on_thread();
+private:
+  int k;
+};
+
+#endif
