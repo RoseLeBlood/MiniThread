61c3be96d0a7485606c7b5808c593d312e60221b
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2020-10-07 09:15:02 +0200

    up css

--- update-docs.sh
+++ update-docs.sh
@@ -1,9 +1,20 @@
 cd doc
 doxygen
+rm -r ../../mnthread-docs/docs/
+
+mkdir -p ../../mnthread-docs/docs/css ../../mnthread-docs/docs/js
+
+cp jquery-2.1.1.min.js ../../mnthread-docs/docs/js/.
+cp bootstrap.min.js ../../mnthread-docs/docs/js/.
+cp bootstrap.min.css ../../mnthread-docs/docs/css/.
+
+mv html/*.css  ../../mnthread-docs/docs/css/.
+mv html/*.js  ../../mnthread-docs/docs/js/.
+
 cp --recursive html/* ../../mnthread-docs/docs/.
 cd ../../mnthread-docs/
 
 git add .
 git commit -m "update version"
-git push
+#git push
 
