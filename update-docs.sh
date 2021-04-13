7425ab2d9ef153911c0a5e7ecc6050e9c80e7b9b
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2021-04-13 15:05:20 +0200

    update to version 2.27.0

--- update-docs.sh
+++ update-docs.sh
@@ -10,7 +10,7 @@ function parse_json() {
 	MN_DEF_VERSION=$(echo "$JSONDATA"|grep -w \"version\" |tail -1 | cut -d\" -f4)
 }
 function create_and_push_doxy() {
-	cd "{$MN_DEF_BUILD_PATH}"
+	cd $MN_DEF_BUILD_PATH
 	doxygen
 
 	cd "{$MN_DEF_BASE_DIR/doc/$MN_DEF_GIT_PATH}"
