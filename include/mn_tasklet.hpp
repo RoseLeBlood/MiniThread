70087a0d1892577d730530d1dbd108b73d9e7ebe
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2021-04-13 15:03:22 +0200

    rename function on_coroutine to on_tasklet

--- include/mn_tasklet.hpp
+++ include/mn_tasklet.hpp
@@ -2,29 +2,30 @@
 *This file is part of the Mini Thread Library (https://github.com/RoseLeBlood/MiniThread ).
 *Copyright (c) 2020 Amber-Sophia Schroeck
 *
-*The Mini Thread Library is free software; you can redistribute it and/or modify  
-*it under the terms of the GNU Lesser General Public License as published by  
+*The Mini Thread Library is free software; you can redistribute it and/or modify
+*it under the terms of the GNU Lesser General Public License as published by
 *the Free Software Foundation, version 3, or (at your option) any later version.
 
-*The Mini Thread Library is distributed in the hope that it will be useful, but 
-*WITHOUT ANY WARRANTY; without even the implied warranty of 
-*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+*The Mini Thread Library is distributed in the hope that it will be useful, but
+*WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 *General Public License for more details.
 *
 *You should have received a copy of the GNU Lesser General Public
 *License along with the Mini Thread  Library; if not, see
-*<https://www.gnu.org/licenses/>.  
+*<https://www.gnu.org/licenses/>.
 */
 #ifndef MINLIB_ESP32_TASKLETS_
 #define MINLIB_ESP32_TASKLETS_
 
+#include "mn_config.hpp"
 
 
 #include "mn_autolock.hpp"
-#include "mn_error.hpp"
-#include "mn_sleep.hpp"
-#include "mn_micros.hpp"
 #include "mn_convar.hpp"
+#include "mn_error.hpp"
+#include "mn_micros.hpp"
+#include "mn_sleep.hpp"
 
 namespace mn {
     /**
@@ -33,17 +34,17 @@ namespace mn {
      *  bottom half processing from an ISR.
      *
      *  This is an abstract base class.
-     *  To use this, you need to subclass it. All of your coroutines should
-     *  be derived from the basic_tasklet class. Then implement the virtual on_coroutine
+     *  To use this, you need to subclass it. All of your tasklet should
+     *  be derived from the basic_tasklet class. Then implement the virtual on_tasklet
      *  function.
-     * 
+     *
      * \ingroup tasklet
      */
 
     class basic_tasklet {
     public:
         basic_tasklet();
-        
+
         /**
          *  schedule this Tasklet to run.
          *
@@ -66,16 +67,16 @@ namespace mn {
          *  You must override this function.
          *
          *  @param parameter Value passed to you from the create() methods.
-         * 
-         * @return false to end the coroutine and true when run 
+         *
+         * @return false to end the on_tasklet and true when run
          */
-        virtual bool on_coroutine(uint32_t arg) = 0;
+        virtual bool on_tasklet(uint32_t arg) = 0;
     protected:
         /**
          *  Adapter function that allows you to write a class
          *  specific on_tasklet() function that interfaces with FreeRTOS.
          */
-    static void runcorostub(void* parm, uint32_t parameter);
+    static void runtasklet(void* parm, uint32_t parameter);
     protected:
         /**
          *  Protect against accidental deletion before we were executed.
--- src/mn_tasklet.cpp
+++ src/mn_tasklet.cpp
@@ -2,21 +2,23 @@
 *This file is part of the Mini Thread Library (https://github.com/RoseLeBlood/MiniThread ).
 *Copyright (c) 2020 Amber-Sophia Schroeck
 *
-*The Mini Thread Library is free software; you can redistribute it and/or modify  
-*it under the terms of the GNU Lesser General Public License as published by  
+*The Mini Thread Library is free software; you can redistribute it and/or modify
+*it under the terms of the GNU Lesser General Public License as published by
 *the Free Software Foundation, version 3, or (at your option) any later version.
 
-*The Mini Thread Library is distributed in the hope that it will be useful, but 
-*WITHOUT ANY WARRANTY; without even the implied warranty of 
-*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+*The Mini Thread Library is distributed in the hope that it will be useful, but
+*WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 *General Public License for more details.
 *
 *You should have received a copy of the GNU Lesser General Public
 *License along with the Mini Thread  Library; if not, see
-*<https://www.gnu.org/licenses/>.  
+*<https://www.gnu.org/licenses/>.
 */
-#include "freertos/FreeRTOS.h"
-#include "freertos/timers.h"
+#include "mn_config.hpp"
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/timers.h>
 
 #include "mn_tasklet.hpp"
 
@@ -24,7 +26,7 @@ namespace mn {
     //-----------------------------------
     //  Construtor
     //-----------------------------------
-    basic_tasklet::basic_tasklet() 
+    basic_tasklet::basic_tasklet()
         : m_ssLock() { }
 
     //-----------------------------------
@@ -33,24 +35,24 @@ namespace mn {
     int basic_tasklet::schedule(uint32_t parameter, TickType_t timeout) {
         BaseType_t success;
 
-        m_ssLock.lock(); 
+        m_ssLock.lock();
 
         if (xPortInIsrContext()) {
             BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-            success = xTimerPendFunctionCallFromISR( runcorostub,
+            success = xTimerPendFunctionCallFromISR( runtasklet,
                                             this, parameter, &xHigherPriorityTaskWoken);
             if(xHigherPriorityTaskWoken)
                 _frxt_setup_switch();
-                        
+
         } else {
-            success = xTimerPendFunctionCall(runcorostub,
+            success = xTimerPendFunctionCall(runtasklet,
                                         this, parameter, timeout);
         }
 
         if (success == pdPASS)
             return ERR_COROUTINE_OK;
-        
+
         m_ssLock.unlock();
         return ERR_COROUTINE_CANSHEDULE;
     }
@@ -63,16 +65,20 @@ namespace mn {
     }
 
     //-----------------------------------
-    //  runcorostub
+    //  runtasklet
     //-----------------------------------
-    void basic_tasklet::runcorostub(void* xHandle, uint32_t parameter) {
+    void basic_tasklet::runtasklet(void* xHandle, uint32_t parameter) {
         basic_tasklet *tasklet = static_cast<basic_tasklet *>(xHandle);
 
-        for(;;) {
-            if (!tasklet->on_coroutine(parameter) )
-                break;
-        } 
+        if(tasklet) {
+			tasklet->m_ssLock.lock();
 
-        tasklet->m_ssLock.unlock();
+			for(;;) {
+				if (!tasklet->on_tasklet(parameter) )
+					break;
+			}
+
+			tasklet->m_ssLock.unlock();
+        }
     }
 }
