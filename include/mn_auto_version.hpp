6c42e892664d4a2deb963acf00195c7e7088d483
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2021-04-28 17:15:36 +0200

    version update

--- include/mn_auto_version.hpp
+++ include/mn_auto_version.hpp
@@ -2,26 +2,26 @@
 #define _MINLIB_AUTO_VERSION_H_
 
 namespace mn{
-
+	
 	//Software Status
-	static const char MN_VERSION_STATUS[] =  "Beta";
-	static const char MN_VERSION_STATUS_SHORT[] =  "b";
-
+	static const char MN_VERSION_STATUS[] =  "Release";
+	static const char MN_VERSION_STATUS_SHORT[] =  "r";
+	
 	//Standard Version Type
 	static const long MN_VERSION_MAJOR  = 2;
-	static const long MN_VERSION_MINOR  = 27;
-	static const long MN_VERSION_BUILD  = 3082;
-	static const long MN_VERSION_REVISION  = 81;
-
+	static const long MN_VERSION_MINOR  = 28;
+	static const long MN_VERSION_BUILD  = 3052;
+	static const long MN_VERSION_REVISION  = 0;
+	
 	//Miscellaneous Version Types
-	static const long MN_VERSION_BUILDS_COUNT  = 28;
-	#define MN_VERSION_RC_FILEVERSION 2,27,3082,81
-	#define MN_VERSION_RC_FILEVERSION_STRING "2, 27, 3082, 81\0"
-	static const char MN_VERSION_FULLVERSION_STRING [] = "2.27.3082.81";
-
+	static const long MN_VERSION_BUILDS_COUNT  = 30;
+	#define MN_VERSION_RC_FILEVERSION 2,28,3052,0
+	#define MN_VERSION_RC_FILEVERSION_STRING "2, 28, 3052, 0\0"
+	static const char MN_VERSION_FULLVERSION_STRING [] = "2.28.3052.0";
+	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long MN_VERSION_BUILD_HISTORY  = 1;
-
+	
 
 }
 #endif //_MINLIB_AUTO_VERSION_H_
