aa6de76c0405948f406afe3a55f6e922069aa167
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2021-05-12 18:01:49 +0200

    add version

--- include/mn_auto_version.hpp
+++ include/mn_auto_version.hpp
@@ -10,14 +10,14 @@ namespace mn{
 	//Standard Version Type
 	#define MN_VERSION_MAJOR  2
 	#define MN_VERSION_MINOR  29
-	#define MN_VERSION_BUILD  8906
-	#define MN_VERSION_REVISION  10
+	#define MN_VERSION_BUILD  8907
+	#define MN_VERSION_REVISION  771
 	
 	//Miscellaneous Version Types
 	#define MN_VERSION_BUILDS_COUNT  53
-	#define MN_VERSION_RC_FILEVERSION 2,29,8906,10
-	#define MN_VERSION_RC_FILEVERSION_STRING "2, 29, 8906, 10\0"
-	#define MN_VERSION_FULLVERSION_STRING  "2.29.8906.10"
+	#define MN_VERSION_RC_FILEVERSION 2,29,8907,771
+	#define MN_VERSION_RC_FILEVERSION_STRING "2, 29, 8907, 771\0"
+	#define MN_VERSION_FULLVERSION_STRING  "2.29.8907.771"
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	#define MN_VERSION_BUILD_HISTORY  0
